668c39e7094f6a3dfffb267870ed9566ae66d87e
Oleg Vakarev <olegvakarev@Olegs-MacBook-Pro.local>
2025-10-26 23:47:01 +0300

    feat: update tailwind

--- internal/web/get_issue_templ.go
+++ internal/web/get_issue_templ.go
@@ -521,7 +521,7 @@ func GetIssueContent(issue *warnly.IssueDetails, user *warnly.User) templ.Compon
 				return templ_7745c5c3_Err
 			}
 		}
-		templ_7745c5c3_Err = templruntime.WriteString(templ_7745c5c3_Buffer, 42, "<div class=\"bg-gray-50 border-l-4 border-blue-500 rounded-lg p-4 mb-6\"><div class=\"flex items-center gap-2 mb-2\"><svg class=\"w-4 h-4 text-blue-500\" fill=\"currentColor\" viewBox=\"0 0 20 20\"><path fill-rule=\"evenodd\" d=\"M18 10a8 8 0 11-16 0 8 8 0 0116 0zm-7-4a1 1 0 11-2 0 1 1 0 012 0zM9 9a1 1 0 000 2v3a1 1 0 001 1h1a1 1 0 100-2v-3a1 1 0 00-1-1H9z\" clip-rule=\"evenodd\"></path></svg><h2 class=\"text-sm font-semibold text-gray-900\">Log Message</h2></div><div class=\"font-mono text-sm text-gray-800\">")
+		templ_7745c5c3_Err = templruntime.WriteString(templ_7745c5c3_Buffer, 42, "<div class=\"bg-gray-50 border-l-4 border-black-500 rounded-lg p-4 mb-6\"><div class=\"flex items-center gap-2 mb-2\"><svg class=\"w-4 h-4 text-blue-500\" fill=\"currentColor\" viewBox=\"0 0 20 20\"><path fill-rule=\"evenodd\" d=\"M18 10a8 8 0 11-16 0 8 8 0 0116 0zm-7-4a1 1 0 11-2 0 1 1 0 012 0zM9 9a1 1 0 000 2v3a1 1 0 001 1h1a1 1 0 100-2v-3a1 1 0 00-1-1H9z\" clip-rule=\"evenodd\"></path></svg><h2 class=\"text-sm font-semibold text-gray-900\">Log Message</h2></div><div class=\"font-mono text-sm text-gray-800\">")
 		if templ_7745c5c3_Err != nil {
 			return templ_7745c5c3_Err
 		}
